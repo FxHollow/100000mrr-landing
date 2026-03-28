8e8ce362705e78d1b6a6c01c7e0c401598657f0a
FxHollow <15584388925@163.com>
2026-03-28 15:15:49 +0800

    Deploy landing page v1
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- landing-page/ui-components/src/components/index.js
+++
@@ -1,8 +0,0 @@
-// Component Library Exports
-// 100000MRR UI Components
-
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Input } from './Input';
-export { default as Badge } from './Badge';
-export { default as Avatar } from './Avatar';
